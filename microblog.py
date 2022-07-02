04e535934f8c02720b665a357daca764c329923a
James Chen <chenjames215@gmail.com>
2022-07-03 01:42:48 +0800

    add database

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+# Add database into flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
